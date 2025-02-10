7d2450a19d9fd4dff3d210d9d4408aef7fce8b67
Marcos Gabriel <marcosgabriel79355@gmail.com>
2025-02-10 09:50:10 -0300

    Initial commit

---
+++ pedidos-api/src/test/java/br/com/marcos/pedidos/api/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.marcos.pedidos.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
