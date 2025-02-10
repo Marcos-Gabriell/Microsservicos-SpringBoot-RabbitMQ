ad1625e444ed21f531b4cbfade7ac48486e441e6
Marcos Gabriel <marcosgabriel79355@gmail.com>
2025-02-10 11:40:58 -0300

    Método de criar pedido

---
+++ pedidos-api/src/main/java/br/com/marcos/pedidos/api/entity/ItemPedido.java
@@ -0,0 +1,19 @@
+package br.com.marcos.pedidos.api.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NonNull;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@NonNull
+@Data
+public class ItemPedido {
+
+    private UUID id = UUID.randomUUID();
+
+    private Produto produto;
+
+    private Integer quantidade;
+}
---
+++ pedidos-api/src/main/java/br/com/marcos/pedidos/api/entity/enums/Status.java
@@ -0,0 +1,6 @@
+package br.com.marcos.pedidos.api.entity.enums;
+
+public enum Status {
+
+    EM_PROCESSAMENTO, PROCESSADO;
+}
